2fa08b043f966f518c9a95e63e6696c307ae6af0
ArianKG <72304422+ArianKG@users.noreply.github.com>
2020-12-21 20:25:41 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-function rgb_to_hex() {
+const btn1 = document.querySelector('.btn1');
+btn1.onclick = reloadElement;
+const btn2 = document.querySelector('.btn2');
+btn2.onclick = reloadElement;
+
+function toHex() {
     var r = +rtxt.value;
     var g = +gtxt.value;
     var b = +btxt.value;
@@ -6,14 +11,14 @@ function rgb_to_hex() {
     var ghex1 = g.toString(16);
     var bhex1 = b.toString(16);
     var hex1 = "Hex: #" + rhex1 + ghex1 + bhex1;
-    p.innerHTML = hex1;
+    txt.innerHTML = hex1;
 }
 
-function hex_to_rgb() {
+function toRGB() {
     var hex2 = hextxt.value;
     var rhex2 = hex2.charAt(0) + hex2.charAt(1);
     var ghex2 = hex2.charAt(2) + hex2.charAt(3);
     var bhex2 = hex2.charAt(4) + hex2.charAt(5);
     var rgb = "RGB: " + rhex2 + ", " + ghex2 + ", " + bhex2;
-    p.innerHTML = rgb;
+    txt.innerHTML = rgb;
 }
